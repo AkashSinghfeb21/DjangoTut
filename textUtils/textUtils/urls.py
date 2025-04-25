b4eac609593dc598bb8d9fc9532837d016c0376a
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2025-04-25 12:08:01 +0530

    25-04-2025

--- textUtils/textUtils/urls.py
+++ textUtils/textUtils/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+    path('capfirst',views.capfirst,name="capfirst"),
+    path('charcount',views.charcount,name="charcount"),
+    path('newlinerremove',views.newlinerremove,name="newlinerremove"),
+    path('spaceremove',views.spaceremove,name="spaceremove"),
+    path('removepunc',views.removepunc,name="removepunc") 
 ]
