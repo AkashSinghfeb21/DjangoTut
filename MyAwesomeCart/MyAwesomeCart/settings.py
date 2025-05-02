6ac0831c0fed0028c4142843aff5521899f889ee
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2025-05-02 12:13:45 +0530

    02-05-2025

--- MyAwesomeCart/MyAwesomeCart/settings.py
+++ MyAwesomeCart/MyAwesomeCart/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'MyAwesomeCart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD':'Wrestlemania35',
+        'HOST':'localhost',
+        'PORT': '3306'
     }
 }
 
---
+++ MyAwesomeCart/shop/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('', views.index,name="ShopHome"),
+    path("about/",views.about,name="about"),
+    path("contact/",views.contact,name="contact"),
+    path("tracker/",views.tracker,name="tracker"),
+    path("search/",views.search,name="search"),
+    path("productview/",views.productView,name="prodView"),
+    path("checkout/",views.checkout,name="checkout"),
+]
